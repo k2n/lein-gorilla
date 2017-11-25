60e3cfcb94a62d50db4af2de1d7bc8a6f0cf6a72
Kenji Nakamura <kenji@signifier.jp>
2017-11-25 15:15:24 +0900

    Applied a patch by behrica in https://github.com/JonyEpsilon/gorilla-repl/pull/275

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.4.0"
+(defproject k2n/lein-gorilla "0.4.1"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.4.0"]]
+  :dependencies [[kixi/gorilla-repl "0.4.1"]]
   :eval-in-leiningen true)
